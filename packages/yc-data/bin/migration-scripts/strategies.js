7547e9c9ee22cd0715d1f823865ff6f34c2b9c0b
OfirYC <ofir@yieldchain.io>
2023-03-31 07:18:13 +0300

    :sparkles: Added statistics schema, dummy data for strats
    Created the statistics table schema & populated it with dummy data for all of the strategies

--- packages/yc-data/bin/migration-scripts/strategies.js
+++ packages/yc-data/bin/migration-scripts/strategies.js
@@ -1,29 +1,49 @@
 import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 const strategies = await client.strategiesv2.findMany();
-// const protocols = await client.protocolsv2.findMany();
-// const strategiesWithNewSteps = strategies.map((strategy) => {
-//   const steps: any[] = strategy.steps as unknown as JSON[];
-//   const newSteps: any[] = [];
-//   for (const step of steps) {
-//     const newProtocolDetails = protocols.find(
-//       (protocol) => protocol.name == step.protocol_details.name
-//     );
-//     newSteps.push({ ...step, protocol_details: newProtocolDetails });
+// const oldFunctions = await client.functions.findMany()
+// const newFlows = await client.flowsv2.findMany();
+// for (const strategy of strategies) {
+//   // Mapping old IDs to new UUIDS
+//   const oldToNewIDs = new Map<number, string>();
+//   // Creating a root step
+//   const rootStep: DBStep = {
+//     id: "root",
+//     parentId: null,
+//     protocol: {} as DBProtocol,
+//     percentage: 100,
+//     inflows: [],
+//     outflows: [],
+//     action: {} as DBAction,
+//     customArgs: [],
+//     function: {} as DBFunction,
+//     children: [],
+//   };
+//   // iterate over each step and map the ID
+//   for (const step of strategy.steps) {
+//     const oldStep = { ...(step as any) } as any;
+//     const newStep = {} as DBStep;
+//     const newID = uuidv4();
+//     oldToNewIDs.set(oldStep.id, newID);
 //   }
-//   return { ...strategy, steps: newSteps };
-// });
-// for (const strategy of strategiesWithNewSteps) {
-//   await client.strategiesv2.update({
-//     where: {
-//       id: strategy.id,
-//     },
-//     data: {
-//       steps: strategy.steps,
-//     },
-//   });
+//   // Another iteration where we now change the details
+//   for (const step of strategy.steps) {
+//     const oldStep = { ...(step as any) } as any;
+//     const newStep: DBStep = {
+//       parentId: oldToNewIDs.get(oldStep.parent_step_identifier) as string,
+//       id: oldToNewIDs.get(oldStep.step_identifier) as string,
+//       protocol: oldStep.protocol_details,
+//       action:
+//     };
+//     if (oldStep.parent_step_identifier === undefined)
+//       newStep.parentId = rootStep.id;
+//   }
 // }
-for (const strategy of strategies) {
-    console.log(strategy.steps);
+for (const strat of strategies) {
+    console.log(strat.steps.map((step) => {
+        return { inflows: step.inflows[0], outflows: step.outflows[0] };
+    }));
+    break;
 }
+// Migrate the steps
 //# sourceMappingURL=strategies.js.map
--- packages/yc-data/src/migration-scripts/strategies.ts
+++ packages/yc-data/src/migration-scripts/strategies.ts
@@ -1,37 +1,64 @@
 import { PrismaClient } from "@prisma/client";
 // @ts-ignore
 import toChecksumAddress from "to-checksum-address";
+import { v4 as uuidv4 } from "uuid";
+import { DBAction, DBFunction, DBProtocol, DBStep } from "@yc/yc-models";
 
 const client = new PrismaClient();
 
 const strategies = await client.strategiesv2.findMany();
-// const protocols = await client.protocolsv2.findMany();
+// const oldFunctions = await client.functions.findMany()
+// const newFlows = await client.flowsv2.findMany();
 
-// const strategiesWithNewSteps = strategies.map((strategy) => {
-//   const steps: any[] = strategy.steps as unknown as JSON[];
-//   const newSteps: any[] = [];
+// for (const strategy of strategies) {
+//   // Mapping old IDs to new UUIDS
+//   const oldToNewIDs = new Map<number, string>();
 
-//   for (const step of steps) {
-//     const newProtocolDetails = protocols.find(
-//       (protocol) => protocol.name == step.protocol_details.name
-//     );
-//     newSteps.push({ ...step, protocol_details: newProtocolDetails });
+//   // Creating a root step
+//   const rootStep: DBStep = {
+//     id: "root",
+//     parentId: null,
+//     protocol: {} as DBProtocol,
+//     percentage: 100,
+//     inflows: [],
+//     outflows: [],
+//     action: {} as DBAction,
+//     customArgs: [],
+//     function: {} as DBFunction,
+//     children: [],
+//   };
+
+//   // iterate over each step and map the ID
+//   for (const step of strategy.steps) {
+//     const oldStep = { ...(step as any) } as any;
+//     const newStep = {} as DBStep;
+
+//     const newID = uuidv4();
+//     oldToNewIDs.set(oldStep.id, newID);
 //   }
 
-//   return { ...strategy, steps: newSteps };
-// });
+//   // Another iteration where we now change the details
+//   for (const step of strategy.steps) {
+//     const oldStep = { ...(step as any) } as any;
 
-// for (const strategy of strategiesWithNewSteps) {
-//   await client.strategiesv2.update({
-//     where: {
-//       id: strategy.id,
-//     },
-//     data: {
-//       steps: strategy.steps,
-//     },
-//   });
+//     const newStep: DBStep = {
+//       parentId: oldToNewIDs.get(oldStep.parent_step_identifier) as string,
+//       id: oldToNewIDs.get(oldStep.step_identifier) as string,
+//       protocol: oldStep.protocol_details,
+//       action:
+//     };
+//     if (oldStep.parent_step_identifier === undefined)
+//       newStep.parentId = rootStep.id;
+//   }
 // }
 
-for (const strategy of strategies) {
-  console.log(strategy.steps);
+for (const strat of strategies) {
+  console.log(
+    strat.steps.map((step: any) => {
+      return { inflows: step.inflows[0], outflows: step.outflows[0] };
+    })
+  );
+
+  break
 }
+// Migrate the steps

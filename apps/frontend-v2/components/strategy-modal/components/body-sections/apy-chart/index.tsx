3e5345a3761bf5d00ab0715d6015205ec0d31017
OfirYC <ofir@yieldchain.io>
2023-04-01 08:33:45 +0300

    :sparkles: APY Chart on strategy modal
    Made the APY chart on the Strategy modal with real data.  Currently looks good w custom tooltip as well (Couldnt use the info provider as entire logic was messing with it so i just copy pasted the div into another tiny component). Gonna add timeframe filtering next

--- apps/frontend-v2/components/strategy-modal/components/body-sections/apy-chart/index.tsx
+++ apps/frontend-v2/components/strategy-modal/components/body-sections/apy-chart/index.tsx
@@ -16,6 +16,10 @@ import { YCStrategy } from "@yc/yc-models";
 import "../../../../../css/globals.css";
 import { Themes, useTheme } from "utilities/hooks/stores/theme";
 import { useEffect, useState } from "react";
+import { InfoProvider } from "components/info-provider";
+import WrappedText from "components/wrappers/text";
+import { YCStatistic } from "@yc/yc-models/src/core/strategy/statistic";
+import { format } from "date-fns";
 
 export const ApyChart = ({ strategy }: { strategy?: YCStrategy }) => {
   console.log("Statistics", strategy?.statistics);
@@ -26,6 +30,8 @@ export const ApyChart = ({ strategy }: { strategy?: YCStrategy }) => {
   useEffect(() => {
     setChartColor(theme == Themes.DARK ? "#ffffff" : "#000000");
   }, [theme]);
+
+  console.log("Strategy Statistics:", strategy?.statistics);
   return (
     <InterModalSection height="h-[30%]" paddingX="px-0" className="pt-6 pb-0">
       <ResponsiveContainer width="150%" height="100%" className="mt-1">
@@ -54,9 +60,42 @@ export const ApyChart = ({ strategy }: { strategy?: YCStrategy }) => {
             tick={{ fontSize: "10px" }}
           />
           <CartesianGrid opacity={0.1} vertical={false} />
-          <Tooltip />
+          <Tooltip
+            content={
+              <CustomTooltip<YCStatistic[]> data={strategy?.statistics} />
+            }
+            wrapperStyle={{ outline: "none" }}
+          />
         </AreaChart>
       </ResponsiveContainer>
     </InterModalSection>
+  );
+};
+
+/**
+ * A custom tooltip wrapper for the chart
+ */
+
+const CustomTooltip = <T extends any[]>({
+  payload,
+  label,
+  active,
+  data,
+}: {
+  payload?: T;
+  label?: number;
+  active?: boolean;
+  data?: T;
+}) => {
+  // If it's not currently active return null so that it is not visible
+  if (!active || !payload || !label || !data) return null;
+
+  return (
+    <div className="flex flex-col bg-custom-componentbg shadow-md px-3 py-1.5 z-[100000] w-max h-max rounded-xl border-1 border-custom-border outline-none ">
+      <WrappedText>{"APY: " + `${payload[0].value}%`}</WrappedText>
+      <WrappedText fontSize={10}>
+        {format(new Date(data[label].timestamp), "MMM do")}
+      </WrappedText>
+    </div>
   );
 };

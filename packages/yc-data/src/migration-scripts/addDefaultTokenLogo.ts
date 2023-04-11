257d2de51039613256c3184d0fe71bc5285b448d
OfirYC <ofir@yieldchain.io>
2023-04-11 11:50:23 +0300

    :sparkles: Added default token logo to all missing tokens in DB

---
+++ packages/yc-data/src/migration-scripts/addDefaultTokenLogo.ts
@@ -0,0 +1,18 @@
+import { PrismaClient, tokensv2 } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const tokens = await prisma.tokensv2.findMany();
+
+const neededtokens: tokensv2[] = [];
+
+await prisma.tokensv2.updateMany({
+  where: {
+    logo: {
+      equals: "undefined",
+    },
+  },
+  data: {
+    logo: "https://etherscan.io/images/main/empty-token.png",
+  },
+});

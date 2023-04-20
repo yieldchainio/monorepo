1658833fc75b5797a09810935d35643637ba1d6b
OfirYC <ofir@yieldchain.io>
2023-04-20 11:40:20 +0300

    :construction_worker: Buitl

--- packages/yc-models/bin/core/action.d.ts
+++
@@ -1,6 +0,0 @@
-import { DBAction } from "../types/db";
-import { YCClassifications } from "./classification";
-export default class YCAction {
-    #private;
-    constructor(_action: DBAction, _context: YCClassifications);
-}

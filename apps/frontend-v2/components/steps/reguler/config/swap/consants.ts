a917e1c7f2e419f26b284c7197d3170f098e02f7
OfirYC <ofir@yieldchain.io>
2023-05-11 17:26:49 +0300

    :coffin: Removed unused constant

--- apps/frontend-v2/components/steps/reguler/config/swap/consants.ts
+++ apps/frontend-v2/components/steps/reguler/config/swap/consants.ts
@@ -5,3 +5,6 @@
 export const SWAP_FUNCTION_ID = "2c0bdd63-4150-4db1-aaac-41a8a1105301";
 
 export const BALANCEOF_FUNCTION_ID = "e265eb5b-e756-437e-86ff-e3ce7dcd0669";
+
+export const CUSTOM_ADDRESS_ARGUMENT_ID =
+  "87142667-cb87-4a3c-a404-d17d034fab17";

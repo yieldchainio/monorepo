fabb58f782d280b8398ad3357e0e274c2fa79e12
OfirYC <ofir@yieldchain.io>
2023-05-25 11:24:37 +0300

    :sparkles: Triggers Engine (New Service)

--- services/triggers-engine/bin/index.js
+++ services/triggers-engine/bin/index.js
@@ -2,13 +2,47 @@
  * Manages triggering strategies
  */
 import { YCClassifications } from "@yc/yc-models";
+import { Contract, Wallet } from "ethers";
+import DiamondABI from "@yc/yc-models/src/ABIs/diamond.json" assert { type: "json" };
+import dotenv from "dotenv";
+dotenv.config();
 const ycContext = YCClassifications.getInstance();
 // Initiallize YC context
 if (!ycContext.initiallized)
     await ycContext.initiallize();
 // Get all the supported networks
 const supportedNetworks = ycContext.networks.filter((network) => network.diamondAddress && network.provider);
+const checkAllStrategiesTriggers = async (diamondContract) => {
+    return (await diamondContract.checkStrategiesTriggers());
+};
+const executeStrategiesTriggers = async (diamondContract, validVaultsIndices, signals) => { };
 for (const network of supportedNetworks) {
-    network.provider.on("block", () => { });
+    const signer = new Wallet(process.env.PRIVATE_KEY || "", network.provider);
+    const diamondContract = new Contract(network.diamondAddress, DiamondABI, signer);
+    const usedBlocks = new Set();
+    network.provider.on("block", async (blockNum) => {
+        console.log("Checking Block #" + blockNum, "On", network.name + "...");
+        if (usedBlocks.has(blockNum))
+            return;
+        usedBlocks.add(blockNum);
+        const indices = [];
+        const strategiesSignals = (await checkAllStrategiesTriggers(diamondContract)).flatMap((signals, index) => {
+            if (signals.includes(true)) {
+                indices.push(index);
+                return [signals];
+            }
+            return [];
+        });
+        if (indices.length == 0)
+            return;
+        console.log("Going To Trigger", indices.length, "Strategies...");
+        try {
+            await diamondContract.executeStrategiesTriggers(indices, strategiesSignals);
+        }
+        catch (e) {
+            const err = e;
+            console.error("Triggers Engine Caught Error Whilst Executing. Cause:", err.cause, "Msg:", err.message, "Stack:", err.stack);
+        }
+    });
 }
 //# sourceMappingURL=index.js.map
--- services/triggers-engine/src/index.ts
+++ services/triggers-engine/src/index.ts
@@ -1,8 +1,11 @@
 /**
  * Manages triggering strategies
  */
-import { YCClassifications } from "@yc/yc-models";
-import { ethers } from "ethers";
+import { SupportedYCNetwork, YCClassifications, address } from "@yc/yc-models";
+import { Contract, Wallet, ethers } from "ethers";
+import DiamondABI from "@yc/yc-models/src/ABIs/diamond.json" assert { type: "json" };
+import dotenv from "dotenv";
+dotenv.config();
 
 const ycContext = YCClassifications.getInstance();
 
@@ -12,8 +15,60 @@ if (!ycContext.initiallized) await ycContext.initiallize();
 // Get all the supported networks
 const supportedNetworks = ycContext.networks.filter(
   (network) => network.diamondAddress && network.provider
-);
+) as SupportedYCNetwork[];
+
+const checkAllStrategiesTriggers = async (diamondContract: Contract) => {
+  return (await diamondContract.checkStrategiesTriggers()) as boolean[][];
+};
+
+const executeStrategiesTriggers = async (
+  diamondContract: Contract,
+  validVaultsIndices: number[],
+  signals: boolean[][]
+) => {};
 
 for (const network of supportedNetworks) {
-  network.provider.on("block", () => {});
+  const signer = new Wallet(process.env.PRIVATE_KEY || "", network.provider);
+  const diamondContract = new Contract(
+    network.diamondAddress,
+    DiamondABI,
+    signer
+  );
+  const usedBlocks = new Set<number>();
+  network.provider.on("block", async (blockNum) => {
+    console.log("Checking Block #" + blockNum, "On", network.name + "...");
+    if (usedBlocks.has(blockNum)) return;
+    usedBlocks.add(blockNum);
+    const indices: number[] = [];
+    const strategiesSignals: boolean[][] = (
+      await checkAllStrategiesTriggers(diamondContract)
+    ).flatMap((signals, index) => {
+      if (signals.includes(true)) {
+        indices.push(index);
+        return [signals];
+      }
+
+      return [];
+    });
+
+    if (indices.length == 0) return;
+
+    console.log("Going To Trigger", indices.length, "Strategies...");
+    try {
+      await diamondContract.executeStrategiesTriggers(
+        indices,
+        strategiesSignals
+      );
+    } catch (e: any) {
+      const err = e as Error;
+      console.error(
+        "Triggers Engine Caught Error Whilst Executing. Cause:",
+        err.cause,
+        "Msg:",
+        err.message,
+        "Stack:",
+        err.stack
+      );
+    }
+  });
 }

1ac872b42bbc6908de6920af0071ea6484a50b9c
Ofir Smolinsky <ofirsmolinsky@Ofirs-MacBook-Pro-2.local>
2023-06-14 09:26:13 +0300

    :construction_worker: Updated build system for apps

---
+++ scripts/build-push-app.js
@@ -0,0 +1,21 @@
+const { exec } = require("child_process");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const args = process.argv;
+const res = exec(
+  `gh workflow run .github/workflows/build-app.yml -f app_name=${args[2]}`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  }
+);
+
+res.on("close", (msg) => console.log(msg));

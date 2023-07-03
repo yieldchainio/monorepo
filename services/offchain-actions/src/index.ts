b54f3ee797f1f3f8365d6fd591e5aa51bc8da518
Ofir Smolinsky <ofirsmolinsky@Ofirs-MacBook-Pro-2.local>
2023-07-03 15:24:40 +0300

    :bricks: Increased Size Limit In Offchain Acitons

--- services/offchain-actions/src/index.ts
+++ services/offchain-actions/src/index.ts
@@ -19,8 +19,12 @@ const app = express();
 
 const PORT = 8080;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+  })
+);
 app.use(cors());
 
 const ycContext = YCClassifications.getInstance();

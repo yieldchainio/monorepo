951c6229048486a4cec1d58b5fb7bb964997d116
Ofir Smolinsky <ofirsmolinsky@Ofirs-MacBook-Pro-2.local>
2023-07-05 08:19:21 +0300

    :bug: Fixed some minor bugs in offchain actions

--- services/offchain-actions/src/utils/is-bytes-like.ts
+++ services/offchain-actions/src/utils/is-bytes-like.ts
@@ -1,3 +1,8 @@
-export const isBytesLike = (param: string): param is `0x${string}` => {
-  return param.slice(0, 2) == "0x" && param.slice(2).length % 32 == 0;
+export const isBytesLike = (param: unknown): param is `0x${string}` => {
+  return (
+    typeof param == "string" &&
+    param.length >= 2 &&
+    param.slice(0, 2) == "0x" &&
+    param.slice(2).length % 32 == 0
+  );
 };

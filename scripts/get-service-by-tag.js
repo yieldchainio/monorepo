7e9dff6a483f451ad82a61f05d3cb8a89b90e5c8
Ofir Smolinsky <ofirsmolinsky@Ofirs-MacBook-Pro-2.local>
2023-07-10 09:22:06 +0300

    :green_heart: Added script to retreive service by tag name

---
+++ scripts/get-service-by-tag.js
@@ -0,0 +1,33 @@
+const { exec, execSync } = require("child_process");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const args = process.argv;
+
+const desiredServiceName = args[2];
+if (!desiredServiceName) throw "[GetServideByTag]: Service ARNs are undefined";
+
+const services = JSON.parse(
+  execSync("aws ecs list-services --cluster YC-Cluster").toString()
+).serviceArns;
+
+for (const serviceArn of services) {
+  const tags = JSON.parse(
+    execSync(
+      "aws ecs list-tags-for-resource --resource-arn " + serviceArn
+    ).toString()
+  ).tags;
+
+  const hasDesiredTag = tags.some(
+    (tag) => tag.key == "SERVICE_NAME" && tag.value == desiredServiceName
+  );
+
+  if (hasDesiredTag) {
+    process.stdout.write(
+      serviceArn.split(
+        "arn:aws:ecs:us-east-1:010073361729:service/YC-Cluster/"
+      )[1]
+    );
+    break;
+  }
+}
